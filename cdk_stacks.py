47b28655a39af828e316a1bddd06f2a846d9421c
gleits77 <gleitman@amazon.com>
2024-06-04 14:58:52 -0700

    Update cdk_stacks.py

--- cdk_stacks.py
+++ cdk_stacks.py
@@ -24,7 +24,7 @@ WebappStack(
     app,
     "PromptChaining-StreamlitWebapp",
     env=env,
-    parent_domain="77testing778766.com",
+    parent_domain="my-domain.com",
 )
 BlogPostStack(
     app,
